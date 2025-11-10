e910e9307b4d6dbc64c5cb25c1578c6ece6edded
Ivan Babiankou <ivan.babiankou@gmail.com>
2025-11-10 20:18:45 +0100

    Build docker image; Configure CI

---
+++ web-app/build.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+set -e
+
+npm run build
+
+# Build and push docker image
+
+IMAGE_NAME="narrator-web-app"
+
+PLATFORMS="${PLATFORMS:-"linux/amd64,linux/arm64"}"
+#PLATFORMS="${PLATFORMS:-"linux/arm64"}"
+COMMIT_HASH=$(git rev-parse --short HEAD)
+TAG="${IMAGE_NAME}-${2:-$COMMIT_HASH}"
+
+echo "Setup things for multiarch build"
+docker buildx inspect multiarch \
+  || docker buildx create --name multiarch --use --driver docker-container
+docker run --rm --privileged tonistiigi/binfmt --install all
+docker buildx inspect --bootstrap
+
+#  --load \
+docker buildx build \
+  --platform "${PLATFORMS}" \
+  --push \
+  -t "ibabiankou/home-lab:${TAG}" .
