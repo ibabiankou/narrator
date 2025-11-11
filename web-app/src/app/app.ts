5bf29eb7a6de1103785f0f89bc6ff4ad7efbc399
Ivan Babiankou <ivan.babiankou@gmail.com>
2025-11-11 18:49:58 +0100

    Add material design; Add toolbar

--- web-app/src/app/app.ts
+++ web-app/src/app/app.ts
@@ -1,9 +1,10 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatToolbar } from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatToolbar],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
