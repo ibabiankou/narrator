4fc7ecf82e4571f7cb00fecd2f8630839e6f6637
Ivan Babiankou <ivan.babiankou@gmail.com>
2025-11-11 18:26:25 +0100

    Add BooksPage and open it by default

--- web-app/src/app/app.routes.ts
+++ web-app/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { BooksPage } from './pages/books/books-page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: 'books', component: BooksPage},
+  {path: '**', redirectTo: '/books'}
+];
