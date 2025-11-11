830592c24d444ace50ab7f69a34bbb0723a4477b
Ivan Babiankou <ivan.babiankou@gmail.com>
2025-11-11 19:00:39 +0100

    Add mat-fab button to bottom right corner

--- web-app/src/app/app.ts
+++ web-app/src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MatToolbar } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MatToolbar],
+  imports: [RouterOutlet, MatToolbar, MatIconModule],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
--- web-app/src/app/pages/books/books-page.ts
+++ web-app/src/app/pages/books/books-page.ts
@@ -1,11 +1,23 @@
 import { Component } from '@angular/core';
+import { MatFabButton } from '@angular/material/button';
+import { MatIcon } from '@angular/material/icon';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-books-page',
-  imports: [],
+  imports: [
+    MatIcon,
+    MatFabButton
+  ],
   templateUrl: './books-page.html',
   styleUrl: './books-page.scss',
 })
 export class BooksPage {
 
+  constructor(private router: Router) {
+  }
+
+  navigateToAdd() {
+    this.router.navigate(['/books/add']);
+  }
 }
