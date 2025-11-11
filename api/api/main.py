2195817b3b40cc42f597f99c5297143845f18f79
Ivan Babiankou <ivan.babiankou@gmail.com>
2025-11-11 14:19:03 +0100

    Configure base url for api.

--- api/api/main.py
+++ api/api/main.py
@@ -1,7 +1,11 @@
-from fastapi import FastAPI
+from fastapi import APIRouter, FastAPI
 
 app = FastAPI()
 
-@app.get("/")
+base_url_router = APIRouter(prefix="/api")
+
+@base_url_router.get("/")
 def read_root():
     return {"Hello": "World"}
+
+app.include_router(base_url_router)
