5e18869522f9c48d36d96dd77b4dc12a499ae710
Ivan Babiankou <ivan.babiankou@gmail.com>
2025-11-11 19:05:14 +0100

    Add empty AddBookPage component

--- web-app/src/app/app.routes.ts
+++ web-app/src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { BooksPage } from './pages/books/books-page';
+import { AddBookPage } from './pages/add-book/add-book-page';
 
 export const routes: Routes = [
   {path: 'books', component: BooksPage},
+  {path: 'books/add', component: AddBookPage},
   {path: '**', redirectTo: '/books'}
 ];
---
+++ web-app/src/app/pages/add-book/add-book-page.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-book-page',
+  imports: [],
+  templateUrl: './add-book-page.html',
+  styleUrl: './add-book-page.scss',
+})
+export class AddBookPage {
+
+}
