035a351564aa426a58a5a7b61dd40fec925ff653
Ivan Babiankou <ivan.babiankou@gmail.com>
2025-11-11 18:05:26 +0100

    Store book details in memory and implement /api/books/:id endpoint

--- api/api/main.py
+++ api/api/main.py
@@ -42,10 +42,17 @@ class BookDetails(BaseModel):
     title: str
     pdf_file_name: str
 
+books = {}
+
 @base_url_router.post("/books/", response_model=BookDetails)
 def create_book(book: CreateBookRequest) -> BookDetails:
     pdf_temp_file = temp_files.get(str(book.pdf_temp_file_id))
     resp = BookDetails(id=book.id, title=book.title, pdf_file_name=pdf_temp_file.filename)
+    books[str(resp.id)] = resp
     return resp
+
+@base_url_router.get("/books/{book_id}", response_model=BookDetails)
+def get_book(book_id: ID) -> BookDetails:
+    return books[str(resp.id)]
 
 app.include_router(base_url_router)
