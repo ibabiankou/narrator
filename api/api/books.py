73d77bc99a719082cb247fd49e94db9f0d7a37a6
Ivan Babiankou <ivan.babiankou@gmail.com>
2025-11-17 13:50:48 +0100

    Upload book file to object store

--- api/api/books.py
+++ api/api/books.py
@@ -1,12 +1,14 @@
 import uuid
 
 from fastapi import APIRouter, HTTPException
+from fastapi.params import Depends
 from pydantic import BaseModel
 from sqlalchemy.exc import IntegrityError
 
 from api import SessionDep
 from api.models import models
 from api.models.models import TempFile
+from api.services.files import FilesService
 
 books_router = APIRouter()
 
@@ -22,13 +24,17 @@ class BookDetails(BaseModel):
     pdf_file_name: str
 
 @books_router.post("/", response_model=BookDetails)
-def create_book(book: CreateBookRequest, session: SessionDep) -> BookDetails:
+def create_book(book: CreateBookRequest, session: SessionDep, files_service: FilesService = Depends()) -> BookDetails:
     # Load temp_file metadata from DB
     pdf_temp_file = session.get(TempFile, book.pdf_temp_file_id)
     if pdf_temp_file is None:
         raise HTTPException(status_code=404, detail="PDF file not found")
 
-    # TODO: Upload file to Object Store.
+    # Upload the book file to the object store
+    try:
+        files_service.store_book_file(book.id, pdf_temp_file)
+    except Exception:
+        raise HTTPException(status_code=500, detail="Failed to store book file")
 
     # Store book metadata in DB
     try:
--- api/api/services/files.py
+++ api/api/services/files.py
@@ -1,7 +1,9 @@
+import logging
 import os
 import uuid
 
 import boto3
+from botocore.exceptions import ClientError
 from dotenv import load_dotenv
 
 from api.models.models import TempFile
@@ -12,36 +14,20 @@ class FilesService:
 
     def __init__(self):
         load_dotenv()
-        s3_client = boto3.client(
+        self.s3_client = boto3.client(
             "s3",
             endpoint_url=os.getenv("S3_ENDPOINT"),
             aws_access_key_id=os.getenv("S3_ACCESS"),
             aws_secret_access_key=os.getenv("S3_SECRET")
         )
-        print("Hello, Amazon S3! Let's list your buckets:")
-
-        # Create a paginator for the list_buckets operation.
-        paginator = s3_client.get_paginator("list_buckets")
-
-        # Use the paginator to get a list of all buckets.
-        response_iterator = paginator.paginate(
-            PaginationConfig={
-                "PageSize": 50,  # Adjust PageSize as needed.
-                "StartingToken": None,
-            }
-        )
-
-        # Iterate through the pages of the response.
-        buckets_found = False
-        for page in response_iterator:
-            if "Buckets" in page and page["Buckets"]:
-                buckets_found = True
-                for bucket in page["Buckets"]:
-                    print(f"\t{bucket['Name']}")
-
-        if not buckets_found:
-            print("No buckets found!")
+        self.bucket_name = os.getenv("S3_BUCKET", "narrator")
 
     def store_book_file(self, book_id: uuid.UUID, book_file: TempFile):
         """Move the book file from the local disk to the object store."""
-        pass
+        remote_file_path = f"{book_id}/{book_file.file_name}"
+
+        try:
+            self.s3_client.upload_file(book_file.file_path, self.bucket_name, remote_file_path)
+        except ClientError as e:
+            logging.error(e)
+            raise e
