2f49069e4ff368b36c89d978e21b65223ad97fd2
Ivan Babiankou <ivan.babiankou@gmail.com>
2025-11-18 22:40:24 +0100

    Drop unnecessary logging and css

--- web-app/src/app/pages/view-book-page/view-book-page.ts
+++ web-app/src/app/pages/view-book-page/view-book-page.ts
@@ -12,7 +12,6 @@ import { BooksService } from '../../core/services/books.service';
   styleUrl: './view-book-page.scss',
 })
 export class ViewBookPage {
-
   private booksService = inject(BooksService);
 
   book = input.required<BookDetails>();
@@ -22,10 +21,8 @@ export class ViewBookPage {
     const bookId = this.book().id;
     const pages = this.pages()
 
-    console.log("Loading more pages...");
     this.booksService.getBookContent(bookId, pages[pages.length-1].index).subscribe(
       content => {
-        console.log("Adding more pages...");
         this.pages.set([...pages, ...content.pages]);
       }
     );
