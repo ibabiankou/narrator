6a4d844a068183a2e2d8a5532dfef851d4cdec3b
Ivan Babiankou <ivan.babiankou@gmail.com>
2025-11-22 14:17:49 +0100

    Add Kokoro dependencies and implement trivial test

--- speech-generator/api/app.py
+++ speech-generator/api/app.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI, APIRouter
+from kokoro import KPipeline
+import soundfile as sf
 
 app = FastAPI()
 
@@ -7,6 +9,20 @@ base_url_router = APIRouter(prefix="/api")
 
 @base_url_router.get("/")
 def health():
+    return {"status": "ok"}
+
+
+pipeline = KPipeline(lang_code='a')
+
+@base_url_router.get("/test")
+def test():
+    text = "Lets test this thing out..."
+
+    generator = pipeline(text, voice='af_heart')
+    for i, (gs, ps, audio) in enumerate(generator):
+        print(i, gs, ps)
+        sf.write(f'{i}.wav', audio, 24000)
+
     return {"status": "ok"}
 
 
