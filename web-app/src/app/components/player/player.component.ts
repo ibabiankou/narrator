45369733b8db05e61a56524fdf6f67653aa6db0a
Ivan Babiankou <ivan.babiankou@gmail.com>
2025-11-24 14:10:24 +0100

    Player WIP

--- web-app/src/app/components/player/player.component.ts
+++ web-app/src/app/components/player/player.component.ts
@@ -1,8 +1,9 @@
-import { Component, input, OnInit, signal } from '@angular/core';
+import { Component, computed, effect, input, OnInit, signal } from '@angular/core';
 import { Section, SpeechStatus } from '../../core/models/books.dto';
 import { MatIcon } from '@angular/material/icon';
 import { MatIconButton } from '@angular/material/button';
 import { environment } from '../../../environments/environment';
+import { interval, take } from 'rxjs';
 
 declare const Amplitude: any;
 
@@ -21,16 +22,65 @@ export class PlayerComponent implements OnInit {
   isPlaying = signal<boolean>(false);
   baseUrl = environment.api_base_url
 
+  readySections = computed(() => this.sections()
+    .filter(section => section.speech_status == SpeechStatus.ready));
+  queuedSections = computed(() => this.sections()
+    .filter(section => section.speech_status == SpeechStatus.queued));
+  missingSections = computed(() => this.sections()
+    .filter(section => section.speech_status == SpeechStatus.missing));
+
+  constructor() {
+    effect(() => {
+      let sectionsInPlaylist = new Set<number>();
+      Amplitude.getSongs().forEach((song: { section_id: number; }) => sectionsInPlaylist.add(song.section_id));
+
+      this.sections()
+        .filter(section => section.speech_status == SpeechStatus.ready)
+        .filter(section => !sectionsInPlaylist.has(section.id))
+        .forEach(section => Amplitude.addSong(
+          {
+            "section_id": section.id,
+            "url": `${this.baseUrl}/books/${section.book_id}/speech/${section.speech_file}`
+          }
+        ))
+    });
+  }
+
   ngOnInit(): void {
     const songs = this.sections().filter(section => section.speech_status == SpeechStatus.ready)
       .map(section => {
         return {
+          "section_id": section.id,
           "url": `${this.baseUrl}/books/${section.book_id}/speech/${section.speech_file}`
         }
       });
-    console.log(songs);
+
     Amplitude.init({
-      songs: songs
+      songs: songs,
+      playback_speed: 1.1
     });
+
+    interval(1000).pipe(take(25))
+      .subscribe(x => console.log("Songs in playlist: ", Amplitude.getSongs().length));
+  }
+
+  // TODO: Implement progress bar. Available, queued, unavailable.
+
+  // TODO: Render current position in time and total playback time.
+  //  How and when calculate this?
+
+  // TODO: Append songs as more content is loaded. < Use state for this?
+
+  // TODO: Auto-scroll to the currently played item. I should probably simply emmit event what is being played
+  //  (upon start and periodically)
+  //  and let the page do the scrolling.
+
+  playPause() {
+    if (this.isPlaying()) {
+      Amplitude.pause();
+    } else {
+      Amplitude.play();
+    }
+    this.isPlaying.update(value => !value);
   }
 }
