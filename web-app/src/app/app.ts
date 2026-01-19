900119cbd5408319d8152a1b29c270cf6f7f5177
Ivan Babiankou <ivan.babiankou@gmail.com>
2026-01-19 15:58:37 +0100

    Build before serving

--- web-app/src/app/app.ts
+++ web-app/src/app/app.ts
@@ -13,5 +13,34 @@ export class App {
   constructor(iconRegistry: MatIconRegistry, injector: Injector) {
     iconRegistry.setDefaultFontSetClass('material-symbols-outlined');
     injector.get(OSBindingsService);
+
+    // this.checkStorage().catch(err => console.error(err));
   }
+
+  // TODO: Collect this info and show on some kind of info page.
+  // async checkStorage() {
+  //   if (navigator.storage && navigator.storage.estimate) {
+  //     const { quota, usage } = await navigator.storage.estimate();
+  //
+  //     const quotaNum = quota ? quota : 0;
+  //     if (quotaNum === 0)
+  //       console.log("Quota is unknown...");
+  //     else
+  //       console.log("Quota is %sB", quotaNum);
+  //
+  //     const usageNum = usage ? usage : 0;
+  //     if (usageNum === 0)
+  //       console.log("Usage is unknown.");
+  //     else
+  //       console.log("Usage is %sB", usageNum);
+  //
+  //     if (quotaNum > 0 && usageNum > 0) {
+  //       const percentageUsed = quotaNum > 0 ? ((usageNum / quotaNum) * 100) : 0;
+  //       const remainingMB = (quotaNum - usageNum) / (1024 * 1024);
+  //
+  //       console.log(`Used: ${percentageUsed.toFixed(2)}%`);
+  //       console.log(`Remaining: ${remainingMB.toFixed(0)} MB`);
+  //     }
+  //   }
+  // }
 }
