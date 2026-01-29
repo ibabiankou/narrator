0e7119f7afc7256d99f69ba743449fafb42c423a
Ivan Babiankou <ivan.babiankou@gmail.com>
2026-01-29 18:57:52 +0100

    Show home button instead of title on a small screen

--- web-app/src/app/components/toolbar/toolbar.component.ts
+++ web-app/src/app/components/toolbar/toolbar.component.ts
@@ -2,6 +2,7 @@ import { Component, ContentChild, Directive, HostListener } from '@angular/core'
 import { MatIcon } from '@angular/material/icon';
 import { MatToolbar } from '@angular/material/toolbar';
 import { RouterLink } from '@angular/router';
+import { MatIconButton } from '@angular/material/button';
 
 @Directive({
   selector: '[breadcrumb]',
@@ -24,6 +25,7 @@ export class ActionButtonContentDirective {
     MatIcon,
     MatToolbar,
     RouterLink,
+    MatIconButton,
   ],
   templateUrl: './toolbar.component.html',
   styleUrl: './toolbar.component.scss',
