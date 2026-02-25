ef1771417bff0bb9f5dab26d3ab1271b4ac1e453
Ivan Babiankou <ivan.babiankou@gmail.com>
2026-02-25 16:47:42 +0100

    Cache settings

--- web-app/src/app/core/services/settings.service.ts
+++ web-app/src/app/core/services/settings.service.ts
@@ -2,35 +2,65 @@ import { Injectable } from '@angular/core';
 import { Settings } from '../models/books.dto';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../../../environments/environment';
-import { BehaviorSubject, filter, tap } from 'rxjs';
+import { BehaviorSubject, filter, switchMap, tap } from 'rxjs';
+import { IndexDBCache } from './indexDBCache';
+import { ConnectionService } from './connection.service';
+import { mergeDeep } from '../utils';
 
 @Injectable({providedIn: 'root'})
 export class SettingsService {
 
   private apiUrl = `${environment.api_base_url}/settings`;
+  private settingsCache: IndexDBCache<Settings>;
 
   private _userPreferences$ = new BehaviorSubject<Settings | undefined>(undefined);
   readonly userPreferences$ = this._userPreferences$.asObservable().pipe(filter(value => value !== undefined));
 
-  constructor(private http: HttpClient) {
+  constructor(private http: HttpClient, private connectionService: ConnectionService) {
+    this.settingsCache = new IndexDBCache(
+      this.connectionService,
+      "settings",
+      (url: string) => {
+        return this.http.get<Settings>(url);
+      },
+      (url: string, payload: Settings) => {
+        return this.http.patch<Settings>(url, payload);
+      });
+
     this.get('user_preferences').subscribe(preferences => this._userPreferences$.next(preferences));
   }
 
   get(kind: string) {
-    return this.http.get<Settings>(`${this.apiUrl}/${kind}`)
+    const url = `${this.apiUrl}/${kind}`;
+    return this.settingsCache.get(url)
       .pipe(tap(patched => {
-        if (kind === 'user_preferences') {
+        if (url.endsWith('user_preferences')) {
           this._userPreferences$.next(patched)
         }
       }));
   }
 
   patch(kind: string, payload: Partial<Settings>) {
-    return this.http.patch<Settings>(`${this.apiUrl}/${kind}`, payload)
-      .pipe(tap(patched => {
-        if (kind === 'user_preferences') {
+    const url = `${this.apiUrl}/${kind}`;
+
+    let setOperation;
+    if (!this.connectionService.isOnline()) {
+      // Handling offline mode explicitly so that multiple consequent patch operations are merged into one
+      // instead of overriding each other.
+      setOperation = this.get(kind).pipe(
+        switchMap(settings => this.settingsCache.set(url, mergeDeep(settings, payload)))
+      )
+    } else {
+      setOperation = this.settingsCache.set(url, payload);
+    }
+
+    return setOperation.pipe(
+      switchMap(() => this.get(kind)),
+      tap(patched => {
+        if (url.endsWith('user_preferences')) {
           this._userPreferences$.next(patched)
         }
-      }));
+      })
+    );
   }
 }
--- web-app/src/app/core/utils.ts
+++ web-app/src/app/core/utils.ts
@@ -48,3 +48,26 @@ export function secondsToTimeFormat(totalSeconds: number): string {
     return `${sign}${mm}:${ss}`;
   }
 }
+
+
+function isObject(item: any): boolean {
+  return (item && typeof item === 'object' && !Array.isArray(item));
+}
+
+/** Deep merges two objects. */
+export function mergeDeep(target: any, source: any): any {
+  let output = Object.assign({}, target);
+  if (isObject(target) && isObject(source)) {
+    Object.keys(source).forEach(key => {
+      if (isObject(source[key])) {
+        if (!(key in target))
+          Object.assign(output, { [key]: source[key] });
+        else
+          output[key] = mergeDeep(target[key], source[key]);
+      } else {
+        Object.assign(output, { [key]: source[key] });
+      }
+    });
+  }
+  return output;
+}
