deddf31de487a2e9cabfbe3b3534c4e443d120b4
Ivan Babiankou <ivan.babiankou@gmail.com>
2026-02-25 01:53:51 +0100

    Make audio louder

--- web-app/src/app/components/player/audio.player.ts
+++ web-app/src/app/components/player/audio.player.ts
@@ -124,7 +124,7 @@ export class AudioPlayer {
         const gainNode = audioContext.createGain();
         source.connect(gainNode);
         gainNode.connect(audioContext.destination);
-        gainNode.gain.value = 1.25;
+        gainNode.gain.value = 1.75;
       });
 
     this.$playbackRate.subscribe(() => {
