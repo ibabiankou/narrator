d6e1aba9cbc6592bac91f098e400647a6bb26d48
Ivan Babiankou <ivan.babiankou@gmail.com>
2026-03-20 23:49:51 +0100

    Consolidate book menu form view-book-page to book-menu

--- web-app/src/app/components/book-menu/book-menu/book-menu.ts
+++ web-app/src/app/components/book-menu/book-menu/book-menu.ts
@@ -1,4 +1,4 @@
-import { Component, computed, inject, input, TemplateRef } from '@angular/core';
+import { Component, computed, inject, input, signal, TemplateRef, viewChild, WritableSignal } from '@angular/core';
 import { MatButtonToggle, MatButtonToggleGroup } from '@angular/material/button-toggle';
 import { MatIcon } from '@angular/material/icon';
 import { MatButton, MatIconButton } from '@angular/material/button';
@@ -12,7 +12,12 @@ import {
 } from '@angular/material/dialog';
 import { SettingsService } from '../../../core/services/settings.service';
 import { BooksService } from '../../../core/services/books.service';
-import { Router } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
+import { OwnerDirective } from '../../../core/ownerDirective';
+import { toObservable } from '@angular/core/rxjs-interop';
+import { filter, interval, Subscription, switchMap, take } from 'rxjs';
+import { BookOverview, BookStats, DownloadInfo } from '../../../core/models/books.dto';
+import { DownloadService } from '../../../core/services/download.service';
 
 @Component({
   selector: 'app-book-menu',
@@ -28,7 +33,9 @@ import { Router } from '@angular/router';
     MatDialogActions,
     MatDialogClose,
     MatDialogContent,
-    MatDialogTitle
+    MatDialogTitle,
+    OwnerDirective,
+    RouterLink
   ],
   templateUrl: './book-menu.html',
   styleUrl: './book-menu.scss',
@@ -36,14 +43,99 @@ import { Router } from '@angular/router';
 export class BookMenu {
   private booksService: BooksService = inject(BooksService);
   private settingsService: SettingsService = inject(SettingsService);
+  private downloadService: DownloadService = inject(DownloadService);
   private dialog = inject(MatDialog);
   private router: Router = inject(Router);
 
-  bookId = input.required<string>();
+  private downloadSubscription: Subscription | null = null;
+
+  bookOverview = input.required<BookOverview>();
+  bookStats = input.required<BookStats>();
   showPages = computed(() => <string>this.settingsService.userPreferences()!["viewer_mode"]);
+
+  downloadInfo: WritableSignal<DownloadInfo | undefined> = signal(undefined);
+  isDownloaded = computed(() => this.downloadInfo() != undefined);
+  isDownloading = computed(() => {
+    const info = this.downloadInfo();
+    if (!info) {
+      return false;
+    }
+    return !(info.fragments_total > 0 && info.fragments_downloaded == info.fragments_total);
+  });
+
+  readonly storageInfoTemplate = viewChild.required('storageInfoTemplate', {read: TemplateRef});
+
+  constructor() {
+    // Continue download if it's not completed.
+    this.downloadSubscription = toObservable(this.downloadInfo)
+      .pipe(
+        take(1),
+        filter(info => !!info),
+        filter(info => info && (info.fragments_total == 0 || info.fragments_total > info.fragments_downloaded)),
+        switchMap(() => this.downloadService.downloadBook(this.bookOverview().id)),
+      )
+      .subscribe({
+        complete: () => {
+          this.downloadSubscription = null;
+          this.reloadDownloadInfo();
+        }
+      });
+  }
 
   setShowPages(viewerMode: string) {
     this.settingsService.patchUserPreferences({viewer_mode: viewerMode});
+  }
+
+  protected downloadBookDialog(templateRef: TemplateRef<any>) {
+    const dialogRef = this.dialog.open(templateRef);
+
+    dialogRef.afterClosed().subscribe(result => {
+      if (result) {
+        this.downloadSubscription = this.downloadService.downloadBook(this.bookOverview().id).subscribe();
+        this.reloadDownloadInfo();
+        this.storageInfoDialog(this.storageInfoTemplate());
+      }
+    });
+  }
+
+  protected storageInfoDialog(templateRef: TemplateRef<any>) {
+    const dialogRef = this.dialog.open(templateRef);
+
+    const reloadInterval = interval(500).subscribe(() => this.reloadDownloadInfo());
+    dialogRef.afterClosed().subscribe(result => {
+      reloadInterval.unsubscribe();
+      if (result) {
+        if (this.downloadSubscription) {
+          this.downloadSubscription.unsubscribe();
+          this.downloadSubscription = null;
+        }
+        this.downloadService.deleteBookData(this.bookOverview().id);
+        this.reloadDownloadInfo();
+      }
+    });
+  }
+
+  private formatter = new Intl.NumberFormat('en-US', {
+    minimumFractionDigits: 0,
+    maximumFractionDigits: 1,
+  });
+
+  protected totalSizeMb() {
+    return this.formatter.format(this.bookStats().total_size_bytes / 1024 / 1024);
+  }
+
+  protected downloadProgressPercent(): string {
+    const info = this.downloadInfo();
+    if (!info || info.fragments_total == 0) {
+      return "0";
+    } else {
+      return this.formatter.format(info.fragments_downloaded / info.fragments_total * 100);
+    }
+  }
+
+  private reloadDownloadInfo() {
+    this.downloadService.getDownloadInfo(this.bookOverview().id)
+      .subscribe(val => this.downloadInfo.set(val));
   }
 
   protected deleteBookDialog(templateRef: TemplateRef<any>) {
@@ -57,7 +149,7 @@ export class BookMenu {
   }
 
   private deleteBook() {
-    this.booksService.delete(this.bookId()).subscribe(
+    this.booksService.delete(this.bookOverview().id).subscribe(
       () => {
         this.router.navigate(['/books']);
       }
--- web-app/src/app/pages/view-book-page/view-book-page.ts
+++ web-app/src/app/pages/view-book-page/view-book-page.ts
@@ -9,77 +9,49 @@ import {
   model,
   QueryList,
   Signal,
-  signal,
-  TemplateRef,
-  viewChild,
-  ViewChildren,
-  WritableSignal
+  ViewChildren
 } from '@angular/core';
-import { BookPage, BookStatus, BookWithContent, DownloadInfo } from '../../core/models/books.dto';
+import { BookPage, BookStatus, BookWithContent } from '../../core/models/books.dto';
 import { BooksService } from '../../core/services/books.service';
-import { BehaviorSubject, filter, interval, Observable, repeat, Subscription, switchMap, take, tap, timer } from 'rxjs';
-import { MatIcon } from '@angular/material/icon';
-import { RouterLink } from '@angular/router';
+import { BehaviorSubject, filter, Observable, repeat, switchMap, take, tap, timer } from 'rxjs';
 import { SectionComponent } from '../../components/section/section.component';
 import { PlayerComponent } from '../../components/player/player.component';
 import { Title } from '@angular/platform-browser';
 import { toObservable, toSignal } from '@angular/core/rxjs-interop';
 import { SkeletonComponent } from '../../components/skeleton/skeleton.component';
-import { MatButton, MatIconButton } from '@angular/material/button';
-import {
-  MatDialog,
-  MatDialogActions,
-  MatDialogClose,
-  MatDialogContent,
-  MatDialogTitle
-} from '@angular/material/dialog';
 import {
   ActionButtonContentDirective,
   BreadcrumbContentDirective,
   ToolbarComponent
 } from '../../components/toolbar/toolbar.component';
-import { DownloadService } from '../../core/services/download.service';
-import { MatMenu, MatMenuItem, MatMenuTrigger } from '@angular/material/menu';
 import { SettingsService } from '../../core/services/settings.service';
 import { HideIdleDirective } from '../../core/hideIdleDirective';
 import { PdfPage } from '../../components/pdf-page/pdf-page';
 import { VisibilityDirective } from '../../core/visibilityDirective';
 import { binarySearch } from '../../core/utils';
-import { OwnerDirective } from '../../core/ownerDirective';
+import { BookMenu } from '../../components/book-menu/book-menu/book-menu';
 
 @Component({
   selector: 'app-view-book-page',
   imports: [
-    MatIcon,
     SectionComponent,
     PlayerComponent,
     SkeletonComponent,
-    MatIconButton,
-    MatDialogTitle,
-    MatDialogContent,
-    MatDialogActions,
-    MatButton,
-    MatDialogClose,
     ToolbarComponent,
     BreadcrumbContentDirective,
     ActionButtonContentDirective,
-    MatMenuTrigger,
-    MatMenu,
-    MatMenuItem,
     HideIdleDirective,
     PdfPage,
     VisibilityDirective,
-    RouterLink,
-    OwnerDirective,
+    BookMenu,
+
   ],
   templateUrl: './view-book-page.html',
   styleUrl: './view-book-page.scss',
 })
 export class ViewBookPage implements AfterViewInit {
   private booksService = inject(BooksService);
-  private downloadService = inject(DownloadService);
   private titleService = inject(Title);
-  private dialog = inject(MatDialog);
   private settingsService: SettingsService = inject(SettingsService);
 
   bookId = input.required<string>();
@@ -89,26 +61,13 @@ export class ViewBookPage implements AfterViewInit {
   pages: Signal<BookPage[]>;
   pagesWindow = model<BookPage[]>([]);
 
-  downloadInfo: WritableSignal<DownloadInfo | undefined> = signal(undefined);
-  isDownloaded = computed(() => this.downloadInfo() != undefined);
-  isDownloading = computed(() => {
-    const info = this.downloadInfo();
-    if (!info) {
-      return false;
-    }
-    return !(info.fragments_total > 0 && info.fragments_downloaded == info.fragments_total);
-  });
-
   settings = toSignal(this.settingsService.userPreferences$);
-
-  private downloadSubscription: Subscription | null = null;
 
   isShowingPages = computed(() => this.settings()!["viewer_mode"] === "both");
 
   protected currentSectionId = 0;
   private $currentSectionId = new BehaviorSubject(0);
 
-  readonly storageInfoTemplate = viewChild.required('storageInfoTemplate', {read: TemplateRef});
   @ViewChildren("section", {"read": ElementRef}) sectionElements!: QueryList<ElementRef>;
 
   constructor() {
@@ -184,21 +143,6 @@ export class ViewBookPage implements AfterViewInit {
           })
         );
     });
-
-    // Continue download if it's not completed.
-    this.downloadSubscription = toObservable(this.downloadInfo)
-      .pipe(
-        take(1),
-        filter(info => !!info),
-        filter(info => info && (info.fragments_total == 0 || info.fragments_total > info.fragments_downloaded)),
-        switchMap(() => this.downloadService.downloadBook(this.bookId())),
-      )
-      .subscribe({
-        complete: () => {
-          this.downloadSubscription = null;
-          this.reloadDownloadInfo();
-        }
-      });
   }
 
   ngAfterViewInit() {
@@ -225,57 +169,5 @@ export class ViewBookPage implements AfterViewInit {
     this.currentSectionId = sectionId;
     this.$currentSectionId.next(sectionId);
     this.scrollToSection(sectionId);
-  }
-
-  protected downloadBookDialog(templateRef: TemplateRef<any>) {
-    const dialogRef = this.dialog.open(templateRef);
-
-    dialogRef.afterClosed().subscribe(result => {
-      if (result) {
-        this.downloadSubscription = this.downloadService.downloadBook(this.bookId()).subscribe();
-        this.reloadDownloadInfo();
-        this.storageInfoDialog(this.storageInfoTemplate());
-      }
-    });
-  }
-
-  protected storageInfoDialog(templateRef: TemplateRef<any>) {
-    const dialogRef = this.dialog.open(templateRef);
-
-    const reloadInterval = interval(500).subscribe(() => this.reloadDownloadInfo());
-    dialogRef.afterClosed().subscribe(result => {
-      reloadInterval.unsubscribe();
-      if (result) {
-        if (this.downloadSubscription) {
-          this.downloadSubscription.unsubscribe();
-          this.downloadSubscription = null;
-        }
-        this.downloadService.deleteBookData(this.bookId());
-        this.reloadDownloadInfo();
-      }
-    });
-  }
-
-  private formatter = new Intl.NumberFormat('en-US', {
-    minimumFractionDigits: 0,
-    maximumFractionDigits: 1,
-  });
-
-  protected totalSizeMb() {
-    return this.formatter.format(this.bookWithContent().stats.total_size_bytes / 1024 / 1024);
-  }
-
-  protected downloadProgressPercent(): string {
-    const info = this.downloadInfo();
-    if (!info || info.fragments_total == 0) {
-      return "0";
-    } else {
-      return this.formatter.format(info.fragments_downloaded / info.fragments_total * 100);
-    }
-  }
-
-  private reloadDownloadInfo() {
-    this.downloadService.getDownloadInfo(this.bookId())
-      .subscribe(val => this.downloadInfo.set(val));
   }
 }
