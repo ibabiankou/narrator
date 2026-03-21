1ee7d0d459108cb743cfad9263daa5bf3db6c07f
Ivan Babiankou <ivan.babiankou@gmail.com>
2026-03-21 02:57:58 +0100

    Configure domain name for local development

--- web-app/src/environments/environment.ts
+++ web-app/src/environments/environment.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: false,
   // api_base_url: 'https://narrator.in.ggnt.eu/api'
   api_base_url: '/api',
-  auth_url_pattern: /^(http:\/\/localhost:4200)?(\/api)(\/.*)?$/i,
+  auth_url_pattern: /^(http(s)?:\/\/laptop.ggnt.eu:4200)?(\/api)(\/.*)?$/i,
 };
