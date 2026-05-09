d4f730ab80bfcd7e0e5f533d2165d3d38027dcfb
Ivan Babiankou <ivan.babiankou@gmail.com>
2026-05-09 17:38:17 +0200

    List all book files

--- api/scripts/parse_epub.py
+++ api/scripts/parse_epub.py
@@ -1,7 +1,11 @@
+import os
 
 dir_with_books = "../../out/epub"
 
 if __name__ == "__main__":
+    for filename in os.listdir(dir_with_books):
+        if filename.endswith(".epub"):
+            print(f"Found EPUB file: {filename}")
 
 
     print("Done...")
