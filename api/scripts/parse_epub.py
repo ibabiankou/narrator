2256aeadc097427d9005db5f94421a11d2f80e8e
Ivan Babiankou <ivan.babiankou@gmail.com>
2026-05-09 20:42:29 +0200

    WIP

--- api/scripts/parse_epub.py
+++ api/scripts/parse_epub.py
@@ -1,4 +1,5 @@
 import os
+from zipfile import ZipFile
 
 dir_with_books = "../../out/epub"
 
@@ -10,8 +11,18 @@ def all_files(dir: str) -> list[str]:
             files.append(abs_file_path)
     return files
 
+def get_root_file():
+    pass
+
 if __name__ == "__main__":
     all_books = all_files(dir_with_books)
     print(f"Found {len(all_books)} books: \n  {"\n  ".join(all_books)}")
+
+    # Start processing each file
+    for book_file in all_books:
+        print("Processing:", book_file)
+        with ZipFile(book_file) as epubf:
+            pass
+
 
     print("Done...")
