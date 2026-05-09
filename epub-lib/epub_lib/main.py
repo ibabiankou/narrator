92a7d32f7851d29e051bd4ab7d3fec8cb309e2ca
Ivan Babiankou <ivan.babiankou@gmail.com>
2026-05-09 22:24:00 +0200

    Get root files for a book.

--- epub-lib/epub_lib/main.py
+++ epub-lib/epub_lib/main.py
@@ -1,7 +1,45 @@
+import logging
 import os
+from logging.config import dictConfig
+from typing import List
 from zipfile import ZipFile
 
+from epub_lib.container import Container
+
+LOGGING_CONFIG = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'standard': {
+            'format': '%(asctime)s [%(levelname)s] %(name)s: %(message)s',
+            'datefmt': '%Y-%m-%d %H:%M:%S'
+        },
+        'detailed': {
+            'format': '%(asctime)s [%(levelname)s] %(name)s:%(lineno)d: %(message)s'
+        },
+    },
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+            'stream': 'ext://sys.stdout',
+            'formatter': 'standard',
+            'level': 'INFO',
+        },
+    },
+    'loggers': {
+        '': {  # Root logger
+            'handlers': ['console'],
+            'level': 'DEBUG',
+        },
+    }
+}
+
+dictConfig(LOGGING_CONFIG)
+LOG = logging.getLogger(__name__)
+
 dir_with_books = "../../out/epub"
+CONTAINER_XML = "META-INF/container.xml"
+
 
 def all_files(dir: str) -> list[str]:
     files = []
@@ -11,18 +49,29 @@ def all_files(dir: str) -> list[str]:
             files.append(abs_file_path)
     return files
 
-def get_root_file():
-    pass
+
+def get_root_files(container_xml) -> List[str]:
+    try:
+        container = Container.from_xml(container_xml)
+        if len(container.root_files.items) > 1:
+            LOG.warning("Got a book with %s root files in container.xml", len(container.root_files.items))
+        return [i.full_path for i in container.root_files.items]
+    except Exception as e:
+        LOG.error("Failed to parse containers.xml", e)
+        return []
+
 
 if __name__ == "__main__":
     all_books = all_files(dir_with_books)
-    print(f"Found {len(all_books)} books: \n  {"\n  ".join(all_books)}")
+    LOG.info("Found %s books: \n  %s", len(all_books), "\n  ".join(all_books))
 
     # Start processing each file
     for book_file in all_books:
-        print("Processing:", book_file)
-        # with ZipFile(book_file) as epubf:
-        #     pass
-
+        LOG.info("Processing: %s", book_file)
+        with ZipFile(book_file) as epubf:
+            LOG.info("Reading and parsing %s", CONTAINER_XML)
+            with epubf.open(CONTAINER_XML) as containerf:
+                root_files = get_root_files(containerf.read())
+                LOG.info("Got %s root files %s", len(root_files), root_files)
 
     print("Done...")
