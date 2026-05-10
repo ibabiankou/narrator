babc7bf365779a5d8700c50a7a4a24c948d07f96
Ivan Babiankou <ivan.babiankou@gmail.com>
2026-05-10 03:30:09 +0200

    Reorder attributes and properties in model

--- epub-lib/tests/model/__init__.py
+++ epub-lib/tests/model/__init__.py
@@ -3,4 +3,12 @@ from xmldiff.main import diff_texts
 
 # noinspection PyTypeChecker
 def assert_no_diff(left, right):
-    assert len(diff_texts(left, right)) == 0
+    diffs = diff_texts(left, right)
+    if diffs:
+        print()
+        print(diffs)
+        print("Left:")
+        print(left)
+        print("Right:")
+        print(right)
+    assert len(diffs) == 0
