4fe0e9fc6761e3cb2cb76cb439698a0a002330bf
Ivan Babiankou <ivan.babiankou@gmail.com>
2026-05-12 13:55:42 +0200

    Log input when fail to parse epub xml

--- epub-lib/epub_lib/epub.py
+++ epub-lib/epub_lib/epub.py
@@ -3,6 +3,8 @@ from os import PathLike
 from typing import IO, List
 from zipfile import ZipFile
 
+from pydantic import ValidationError
+
 from epub_lib.model.container import CONTAINER_XML, Container
 from epub_lib.model.package import Package
 
@@ -21,11 +23,20 @@ class Epub:
     def _get_root_files(self, epub: ZipFile) -> List[str]:
         LOG.debug("Reading and parsing %s", CONTAINER_XML)
         with epub.open(CONTAINER_XML) as container_file:
-            container = Container.from_xml(container_file.read())
+            try:
+                raw_bytes = container_file.read()
+                container = Container.from_xml(raw_bytes)
+            except ValidationError as e:
+                LOG.debug("Raw contents of %s :\n%s", CONTAINER_XML, raw_bytes.decode())
+                raise e
             return [i.full_path for i in container.root_files.items]
 
     def _get_package(self, epub: ZipFile, root_file: str) -> Package:
         LOG.debug("Reading and parsing root file %s", root_file)
         with epub.open(root_file) as package_file:
             package_xml = package_file.read()
-            return Package.from_xml(package_xml)
+            try:
+                return Package.from_xml(package_xml)
+            except ValidationError as e:
+                LOG.debug("Raw contents of %s :\n%s", root_file, package_xml.decode())
+                raise e
