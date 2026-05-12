963be0719babe0dbf9ad1610ff3d1721d825a90e
Ivan Babiankou <ivan.babiankou@gmail.com>
2026-05-12 13:23:40 +0200

    Tweak script to upload more books

--- api/scripts/upload_epub_files.py
+++ api/scripts/upload_epub_files.py
@@ -5,14 +5,15 @@ from scripts.client import NNarrator
 
 LOG = logging.getLogger(__name__)
 
-dir_with_books = "../../out/epub"
-
 
 def all_files(dir: str) -> list[str]:
+    dir = os.path.expanduser(dir)
+    dir = os.path.abspath(dir)
+    LOG.info("Scanning %s for epub files...", dir)
     files = []
     for filename in os.listdir(dir):
         if filename.endswith(".epub"):
-            abs_file_path = os.path.abspath(os.path.join(dir, filename))
+            abs_file_path = os.path.join(dir, filename)
             files.append(abs_file_path)
     return files
 
@@ -20,9 +21,15 @@ def all_files(dir: str) -> list[str]:
 if __name__ == "__main__":
     narrator = NNarrator("https://laptop.ggnt.eu:4200/api/")
 
-    all_books = all_files(dir_with_books)
-    LOG.info("Found %s books: \n  %s", len(all_books), "\n  ".join(all_books))
-    for book in all_books:
+    all_books = []
+    all_books.extend(all_files("../../out/epub"))
+    all_books.extend(all_files("~/data"))
+
+    subset = all_books[:50]
+
+    LOG.info("Found %s books but will process only %s.", len(all_books), len(subset))
+    LOG.info("  %s", "\n  ".join(all_books))
+    for book in subset:
         LOG.info("Uploading %s", book)
         narrator.procurement_upload(book)
 
