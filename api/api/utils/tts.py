1bebd961974a63d5e678107a529e58ab10ed3b37
Ivan Babiankou <ivan.babiankou@gmail.com>
2026-07-03 21:51:36 +0200

    WIP: Add FragmentInjectorV2

--- api/api/utils/tts.py
+++ api/api/utils/tts.py
@@ -171,6 +171,7 @@ class FragmentInjector:
 
     def traverse(self, node):
         self.open_fragment_if_needed()
+        LOG.debug("  " * len(self.open_tag_stack) + "Traversing node of type: %s", type(node))
 
         if isinstance(node, str):
             node_tokens = [Token(t) for t in tokenize_with_whitespace(node)]
@@ -257,3 +258,161 @@ def process_xhtml_inplace(file_bytes: bytes, global_id_start) -> Tuple[bytes, Fr
         raise e
 
     return soup.encode(formatter="minimal", encoding='utf-8'), fragments.build(), fragments.current_id
+
+
+class FragmentInjectorV2:
+    # The fragment markup is always at the root level of the tag.
+
+    def __init__(self,
+                 tag: Tag,
+                 fragments: FragmentGroupsBuilder,
+                 visited_ids: Optional[Set[str]] = None,
+                 target_length: int = 75):
+        # Updated content of the tag to be concatenated.
+        self.new_content = ["<body>"]
+        self.new_children = []
+
+        self.tag = tag
+        self.fragments = fragments
+        self.target_length = target_length
+
+        # TODO: make sure to push / pop IDs encountered during tag traversal.
+        self.visited_ids = visited_ids or set()
+
+        # Unprocessed tokens in the current fragment.
+        self.frag_q = deque[Token]()
+        # Unprocessed tokens in the current tag.
+        self.tag_q = deque[Token]()
+
+        self.pending_fragments: deque[List[Token]] = deque()
+        self.current_fragment = None
+
+        self.open_tag_stack = []
+
+    def inject(self):
+        # Split the content into fragments.
+        tag_tokens = tokenize_tag_content(self.tag)
+        if self.only_empty(tag_tokens):
+            return
+
+        if tag_tokens:
+            # Assuming that injection only happens on block elements such as paragraph or div.
+            # Therefore, we want to ensure punctuation is present at the end to have an appropriate pause.
+            tag_tokens[-1].add_punctuation_in_tts = True
+
+        self.fragments.next_group()
+
+        if self._scene_break(tag_tokens):
+            self.fragments.add_pause(1, [])
+            return
+
+        self.pending_fragments.extend(split_tokens_into_fragments(tag_tokens, target_length=self.target_length))
+        expected_fragment_number = len(self.pending_fragments)
+
+        for child in self.tag.contents:
+            self.traverse(child)
+        # self.new_content.append(f"</span>")
+
+        # Temporary sanity check if the number of fragments in the current group matches the number of pending fragments
+        actual_fragment_number = self.fragments.current_group_size()
+        if actual_fragment_number != expected_fragment_number:
+            LOG.warning("Expected %d fragments, but got %d", expected_fragment_number, actual_fragment_number)
+
+        # TODO: The following part is weird as fuck! I wonder if I can build beautiful soup model right away
+        #  and avoid entire string concatenation and parsing shenanigans.
+        # self.new_content.append("</body>")
+        # new_soup = BeautifulSoup("".join(self.new_content), 'xml')
+        self.tag.clear()
+        if self.new_children:
+            for child in self.new_children: self.tag.append(child)
+
+    def only_empty(self, tokens: List[Token]):
+        for t in tokens:
+            if not t._tts_text.isspace():
+                return False
+        return True
+
+    @staticmethod
+    def _scene_break(tokens: List[Token]):
+        """Returns True if we consider tokens to represent a scene break.
+        Usually it's a number of repetitive non-word characters."""
+        char_dict = defaultdict[str, int](int)
+        total_count = 0
+        for t in tokens:
+            for c in t.raw_text:
+                if c.isspace(): continue
+                char_dict[c] += 1
+                total_count += 1
+        # Check if all the characters are non word characters.
+        if len(char_dict) > 0 and total_count > 0:
+            for character, count in char_dict.items():
+                if character.isalnum():
+                    return False
+            return True
+        return False
+
+    def traverse(self, node):
+        self.open_fragment_if_needed()
+        LOG.debug("  " * len(self.open_tag_stack) + "Traversing node of type: %s", type(node))
+
+        if isinstance(node, str):
+            node_tokens = [Token(t) for t in tokenize_with_whitespace(node)]
+            self.tag_q.extend(node_tokens)
+
+            while len(self.tag_q) > 0:
+                tag_tok = self.tag_q.popleft()
+                frag_tok = self.frag_q.popleft()
+                if tag_tok.normalized_text != frag_tok.normalized_text:
+                    # This should not be happening, because both fragments and these tokens are collected the same way.
+                    raise ValueError(f"Token mismatch: '{tag_tok}' != '{frag_tok}'")
+
+                self.new_content.append(tag_tok.raw_text)
+
+                if len(self.frag_q) == 0 and len(self.tag_q) > 0:
+                    # End of the fragment is reached, but we have more content here, so open another one.
+                    self.open_fragment_if_needed()
+
+        elif node.name:  # TODO: figure out where name is added and do a nicer check to keep the type info.
+            # Current node is a tag node, open in and add to the open_tag_stack
+
+            attrs = {k: " ".join(v) if isinstance(v, list) else v for k, v in node.attrs.items()}
+            attr_str = " ".join([f'{k}="{v}"' for k, v in attrs.items()])
+            tag_open = f"<{node.name} {attr_str}>" if attr_str else f"<{node.name}>"
+
+            LOG.debug("  " * len(self.open_tag_stack) + "Traversing: %s", node.name)
+
+            if node.name in VOID_TAGS:
+                self.new_content.append(tag_open.replace(">", " />"))
+            else:
+                self.open_tag_stack.append((node.name, attrs))
+                self.new_content.append(tag_open)
+                for child in node.contents:
+                    # If the current fragment queue is empty, then open the next fragment.
+                    self.traverse(child)
+                self.new_content.append(f"</{node.name}>")
+                self.open_tag_stack.pop()
+        else:
+            LOG.warning("  " * len(self.open_tag_stack) + "  >>> Not a string and has no name: %s", node)
+            raise RuntimeError("Unexpected node type")
+
+    def open_fragment_if_needed(self):
+        if len(self.frag_q) == 0 and len(self.pending_fragments) > 0:
+            next_fragment = self.pending_fragments.popleft()
+            self.frag_q.extend(next_fragment)
+
+            # Close the current fragment, if present
+            if self.current_fragment is not None:
+                # TODO: instead, create a new copy of the stack, and append it to the new_children. So this block is not even needed.
+                #  Rather, I need to modify the last block to create a next child and copy the open tag stack, if any.
+                for t_name, _ in reversed(self.open_tag_stack):
+                    self.new_content.append(f"</{t_name}>")
+                self.new_content.append(f"</span>")
+
+            added_fragment = self.fragments.add_tokens(next_fragment, list(self.visited_ids))
+            self.current_fragment = added_fragment
+
+            # Open newly added fragment and re-open all tags.
+            self.new_content.append(new_span(added_fragment.formatted_id()))
+            for t_name, t_attrs in self.open_tag_stack:
+                attr_str = " ".join([f'{k}="{v}"' for k, v in t_attrs.items()])
+                self.new_content.append(f"<{t_name} {attr_str}>" if attr_str else f"<{t_name}>")
