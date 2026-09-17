3ccbcfe317bd4c8cd9b0de0eb42e5f09feb2cb79
Ivan Babiankou <ivan.babiankou@gmail.com>
2026-09-17 20:24:24 +0200

    Test problematic book can be processed

--- api/tests/services/test_epub.py
+++ api/tests/services/test_epub.py
@@ -116,3 +116,28 @@ class TestEpubService:
             narration_manifest_file_name = dest_dir_path.joinpath(epub_path.stem + "_narration_manifest.json")
             with open(narration_manifest_file_name, "w") as f:
                 f.write(narration_manifest.model_dump_json(indent=2))
+
+    def test_successful_narration_manifest_creation(self, test_data_loader):
+        file_name = "empire-rising-6.epub"
+        file_name_no_ext = file_name.split(".")[0]
+        epub_bytes = test_data_loader(file_name, text=False)
+        dest_dir_path = Path(os.path.expanduser("~/repos/narrator/out/fragments/"))
+
+        epub = Epub(epub_bytes)
+
+        clean_epub = svc.remove_links(epub_bytes)
+        epub_with_fragments, fragments = svc.inline_fragments(clean_epub)
+
+        epub_file_name = dest_dir_path.joinpath(file_name_no_ext + "_updated.epub")
+        with open(epub_file_name, "wb") as f:
+            f.write(epub_with_fragments.getvalue())
+
+        fragments_file_name = dest_dir_path.joinpath(file_name_no_ext + "_fragments.json")
+        with open(fragments_file_name, "w") as f:
+            json.dump({k: v.model_dump() for k, v in fragments.items()}, f, indent=2)
+
+        epub_extracted_path = dest_dir_path.joinpath(file_name_no_ext + "_unpacked")
+        zipfile.ZipFile(epub_with_fragments).extractall(path=epub_extracted_path)
+
+        publication_content = epub.get_publication_content()
+        narration_manifest = svc.build_narration_manifest(publication_content, fragments)
